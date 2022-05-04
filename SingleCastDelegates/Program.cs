5c47e4a8e663ba03e03a7481d6aa2bdffa17b815
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-05-04 16:45:24 +0530

    [VINAY] Add: Addition of two Elements Using Delegates

---
+++ SingleCastDelegates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using SingleCastDelegates;
+using static SingleCastDelegates.SingleCastDelegate;
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        Operation obj = new Operation(Addition);
+        Console.WriteLine("Asdditin of the two  number is:-> " + obj(51, 10));
+    }
+}
